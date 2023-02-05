f778eade73f304449626b283ce2f17e557e1b5ef
Renqiu Chen <renqiuchen1999@126.com>
2023-02-05 12:30:35 -0500

    Update LegoBlock.cs

--- Assets/LegoBlock.cs
+++ Assets/LegoBlock.cs
@@ -1,10 +1,12 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class LegoBlock : Block
 {
     public List<Vector3> activatedList;
+    private bool _legoFlag=true;
     private void OnTriggerStay(Collider other)
     {
         if (other.tag == "Rubik")
@@ -12,14 +14,19 @@ public class LegoBlock : Block
             currentRubik = other.GetComponent<Rubik>();
             if (other.transform.position - this.transform.position == Vector3.zero)
             {
-                currentRubik.blockFlag = true;  
+                currentRubik.blockFlag = true;
                 MeshRenderer.material = stayMaterial;
-                EnableCubes();
+                if (_legoFlag)
+                {
+                    EnableCubes();
+                    _legoFlag = false;
+                }
             }
             else
             {
                 MeshRenderer.material = exitMaterial;
                 currentRubik.blockFlag = false;
+                _legoFlag = true;
             }
         }
     }
