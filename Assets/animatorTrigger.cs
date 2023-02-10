a9fe220b0c8d493c4898b06cfbe9f710653036c3
Renqiu Chen <renqiuchen1999@126.com>
2023-02-09 23:48:29 -0500

    Update rotate

--- Assets/animatorTrigger.cs
+++ Assets/animatorTrigger.cs
@@ -5,17 +5,19 @@ using UnityEngine;
 public class animatorTrigger : MonoBehaviour
 {
     public GameObject particleEffect;
+    public bool isLego;
     public LegoBlock block;
     public void ParticaleSwitchOff()
     {
         particleEffect.SetActive(false);
-        block.EnableCubes();
-        Debug.Log("enabled");
+        if (isLego)
+        {
+            block.EnableCubes();
+        }
     }
 
     public void ParticaleSwitchOn()
     {
         particleEffect.SetActive(true);
-
     }
 }
